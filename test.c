66da6703a16f120070c9f5d7d3d042df301cc36a
Lolirot <innavsslv@gmail.com>
2015-01-25 19:54:00 +0200

    InsertKey working

--- test.c
+++ test.c
@@ -4,13 +4,20 @@
 
 void main()
 {
-  bintree *tree;
+ // bintree *tree;
   bintree *tree1;
-  bintree *tree2;
+ // bintree *tree2;
   tree1 = mkNode(5,5,NULL,NULL);
-  tree2 = mkNode(7,7,NULL,NULL);
-  tree = mkNode(6,6, tree1, tree2);
+ // tree2 = mkNode(7,7,NULL,NULL);
+  //tree = mkNode(6,6, tree1, tree2);
 
-  printBinTree(tree);
+
+  printBinTree(tree1);
+  printf(",Inserting key six, value six\n");
+  insertKey(6,6, tree1);
+  printBinTree(tree1);
+  printf("Inserting key four value four\n");
+    insertKey(4,4, tree1);
+  printBinTree(tree1);
   
   }
