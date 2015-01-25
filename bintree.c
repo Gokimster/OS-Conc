16d7a2cb1e66cdd1b9412c3a45dd7cf8ea0ae32d
Tamara Tanasescu <gokimster@yahoo.com>
2015-01-25 13:57:00 +0000

    First Commit

---
+++ bintree.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bintree.h"
+
+struct BINTREE {
+  int key;
+  int value;
+  bintree *left;
+  bintree *right;
+} ;
+
+bintree *mkNode( int key, int value, bintree *left, bintree *right)
+{
+  BINTREE bt;
+  bt.key = key;
+  bt.value = value;
+  bt.left = left;
+  bt.right = right;
+  return bt
+}
+
+void freeNode( bintree * tree)
+{
+  /* fill in here */
+}
+
+void freeBinTree( bintree *tree)
+{
+  /* fill in here */
+}
+
+static
+void indent( int off)
+{
+  int i;
+
+  for( i=0; i<off; i++) {
+    printf( " ");
+  }
+}
+
+static
+void printBinTreeOff( int off, bintree *tree)
+{
+  if (tree != NULL) {
+    printf( "Node{ key:%d value:%d\n", tree->key, tree->value);
+    off += 6;
+    indent( off);
+    printf( "left: ");
+    printBinTreeOff( off+6, tree->left);
+    indent( off);
+    printf( "right:");
+    printBinTreeOff( off+6, tree->right);
+    indent( off-2);
+    printf( "}\n");
+  } else {
+    printf( "NULL\n");
+  }
+}
+
+void printBinTree( bintree *tree)
+{
+  printBinTreeOff( 0, tree);
+}
+
+bintree *mergeBinTrees( bintree *tree1, bintree *tree2)
+{
+  /* fill in here */
+}
+
+bintree *insertKey( int key, int value, bintree *tree)
+{
+  /* fill in here */
+}
+
+bintree *deleteKey( int key, bintree *tree)
+{
+  /* fill in here */
+}
+
+int findKey( int key, bintree *tree)
+{
+  /* fill in here */
+}
+
